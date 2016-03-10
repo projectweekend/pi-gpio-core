921c124e95e05ff09ea0125e2a72248e08da667b
Brian Hines <brian@projectweekend.net>
2016-03-10 15:07:29 -0600

    Start client work

--- pi_gpio_core/tests.py
+++ pi_gpio_core/tests.py
@@ -1,13 +1,14 @@
 import unittest
-from gpiozero import DigitalInputDevice
-from .gpio import PINS, add_input
+import gpiozero
+from .gpio import GpioZeroManager
 
 
 class GpioTestCase(unittest.TestCase):
 
     def setUp(self):
         super(GpioTestCase, self).setUp()
+        self.gpio_manager = GpioZeroManager(gpio_lib=gpiozero)
 
     def test_add_input(self):
-        add_input(pin=10)
-        self.assertIsInstance(PINS[10], DigitalInputDevice)
+        self.gpio_manager.add_input(pin=10)
+        self.assertIsInstance(self.gpio_manager.pins[10], gpiozero.DigitalInputDevice)
